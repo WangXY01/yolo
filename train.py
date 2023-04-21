2843b290023a09a9f5550adfaea7ddfd85f13ba3
WangXiaoYu <53108699+WangXY01@users.noreply.github.com>
2023-04-21 21:55:30 +0800

    Update train.py

--- train.py
+++ train.py
@@ -526,7 +526,7 @@ def train(hyp, opt, device, tb_writer=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolo7v7-tiny.pt', help='initial weights path')
+    parser.add_argument('--weights', type=str, default='yolov7-tiny.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='cfg/training/yolov7-tiny.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/mydata.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.p5.yaml', help='hyperparameters path')
